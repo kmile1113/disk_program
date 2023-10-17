fc218355777f1eacd0ee5b1450a43eed2153c869
kmile1113 <146053204+kmile1113@users.noreply.github.com>
2023-10-17 19:30:53 +0800

    文件网盘

---
+++ my.h
@@ -0,0 +1,20 @@
+#ifndef _MY_H
+#define _MY_H
+//如何避免头文件的重复包含
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sqlite3.h>
+#endif
